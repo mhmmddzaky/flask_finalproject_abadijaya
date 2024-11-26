012b0020b2308c4bb3ea1a4f2bc34e3a1229c89c
mhmmddzaky <muh.dzaky1919@gmail.com>
2024-11-26 19:33:36 +0700

    init projek folder

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# MONGODB_URI = os.environ.get("MONGODB_URI")
+# DB_NAME = os.environ.get("DB_NAME")
+
+# client = MongoClient(MONGODB_URI)
+# db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
